3d5714ad4a4e7bdba1707a89f027159739418a56
ponomap26 <ponomap26@yandex.ru>
2023-03-25 15:57:09 +0400

    ver 1

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import telebot
+from config import keys
+from extensions import APIException, MoneyConvertor
+load_dotenv(find_dotenv())
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+
+
+@bot.message_handler(commands=['start', 'help'])
+def beginning(message: telebot.types.Message):
+    text = "Что бы начать работу введите команду боту в следующем формате:\n<имя валюты>\
+<В какую валюту перевести>\
+<Количество переводимой валюты>\n Список доступных валют: /values"
+
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def date(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def get_price(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise APIException("В Ведите 3 параметра. /help")
+        quote, base, amount = values
+        quote, base, = quote.lower(), base.lower()
+        total = MoneyConvertor.convert(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка пользователя\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удается обработать команду\n{e} /help")
+    else:
+        text = f"Цена {amount} {quote}  в  {base} - {total}"
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'биткойн': 'BTC'
+}
+# TOKEN = "5843056810:AAGG2_kH2eJRCwH4vkbczjVeRLgC9Cp_jAY"
